7175ca6bfe45025d2940c8046ac739374e2442ea
Kane_Hiroshi <lksky88@sina.cn>
2023-03-27 16:09:31 +0800

    first version of playable code

---
+++ catch_chiguo/catch_chiguo.py
@@ -0,0 +1,161 @@
+import cv2 as cv
+import numpy as np
+import os
+from match_template_func import get_coor
+from keymouse import move_close_to
+from keymouse import move_and_click_in_game
+
+catch_path = "./catch"
+run_path = "./run"
+template_catch_list = []
+template_run_list = []
+
+# 参数设置
+CONFIDENCE = 0.5  # 过滤弱检测的最小概率
+THRESHOLD = 0.4  # 非最大值抑制阈值
+boxThreshold = 0.5
+classThreshold = 0.5
+nmsThreshold = 0.45
+nmsScoreThreshold = boxThreshold * classThreshold
+
+yolo_labels = ['chiguo']
+netAnchors = [[10, 13, 16, 30, 33, 23], [30, 61, 62, 45, 59, 119 ], [116, 90, 156, 198, 373, 326]]
+netStride = [8.0, 16.0, 32.0, 64.0]
+netWidth = 416
+netHeight = 416
+
+# COLORS = np.random.randint(0, 255, size=(len(yolo_labels), 3), dtype="uint8")
+net = None
+
+
+def load_catch_and_run_template():
+    template_catch_list.clear()
+    catch_pics = os.listdir(catch_path)
+    for i in range(len(catch_pics)):
+        catch_pic = catch_pics[i]
+        template_catch = cv.imread(os.path.join(catch_path, catch_pic), 0)
+        template_catch_list.append(template_catch)
+    template_run_list.clear()
+    run_pics = os.listdir(run_path)
+    for i in range(len(run_pics)):
+        run_pic = run_pics[i]
+        template_run = cv.imread(os.path.join(run_path, run_pic), 0)
+        template_run_list.append(template_run)
+
+
+def net_load():
+    global net
+    net = cv.dnn.readNet('chiguo.onnx')
+    net = cv.dnn.readNet('chiguo.onnx')
+    net.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
+    # net.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+    # net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+    # net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+def predict(net, img, labels):
+    (H, W) = img.shape[:2]
+    blobImg = cv.dnn.blobFromImage(img, 1.0/255.0, (netWidth, netHeight), None, True, False)
+    net.setInput(blobImg)  # # 调用setInput函数将图片送入输入层
+    outInfo = net.getUnconnectedOutLayersNames()
+    layerOutputs = net.forward(outInfo)
+    
+    ratio_h = H / netHeight
+    ratio_w = W / netWidth
+    
+    boxes = []  # 所有边界框
+    confidences = []  # 所有置信度
+    classIDs = []  # 所有分类ID
+    centers = []  # 所有中心
+    
+    net_width = 5 + len(yolo_labels)
+    # 筛选置信度
+    for stride in range(len(layerOutputs)):  # 对每个stride
+        grid_x = int(netWidth / netStride[stride])
+        grid_y = int(netHeight / netStride[stride])
+        for anchor in range(3):  # 对每个anchor框
+            anchor_w = netAnchors[stride][anchor * 2]
+            anchor_h = netAnchors[stride][anchor * 2 + 1]
+            out = layerOutputs[stride][0][anchor]
+            out = 1.0 / (1.0 + np.exp(-out))  # 整体sigmoid
+            for i in range(grid_y):
+                for j in range(grid_x):
+                    box_score = out[i,j,4]
+                    if (box_score >= boxThreshold):
+                        scores = out[i,j,5:]
+                        classID = np.argmax(scores)
+                        max_class_score = scores[classID]
+                        if (max_class_score >= classThreshold):
+                            x = (out[i,j,0] * 2 - 0.5 + j) * netStride[stride] * ratio_w
+                            y = (out[i,j,1] * 2 - 0.5 + i) * netStride[stride] * ratio_h
+                            w = (out[i,j,2] * 2) ** 2 * anchor_w * ratio_w
+                            h = (out[i,j,3] * 2) ** 2 * anchor_h * ratio_h
+                            left = int(x - 0.5 * w + 0.5)
+                            top = int(y - 0.5 * h + 0.5)
+                            classIDs.append(classID)
+                            confidences.append(max_class_score * box_score)
+                            boxes.append([left, top, int(w), int(h)])
+                            centers.append([left + int(w/2), top + int(h/2)])
+    
+    # 非最大值抑制
+    idxs = cv.dnn.NMSBoxes(boxes, confidences, nmsScoreThreshold, nmsThreshold)  # boxes中，保留的box的索引index存入idxs
+    out_boxes = []  # 所有边界框
+    out_confidences = []  # 所有置信度
+    out_classIDs = []  # 所有分类ID
+    out_centers = []  # 所有中心
+
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            out_boxes.append(boxes[i])
+            out_confidences.append(confidences[i])
+            out_classIDs.append(classIDs[i])
+            out_centers.append(centers[i])
+    #     for i in idxs.flatten():  # indxs是二维的，第0维是输出层，所以这里把它展平成1维
+    #         (x, y) = (boxes[i][0], boxes[i][1])
+    #         (w, h) = (boxes[i][2], boxes[i][3])
+    #         color = [int(c) for c in COLORS[classIDs[i]]]
+    #         cv.rectangle(img, (x, y), (x+w, y+h), color, 2)
+    #         text = "{}: {:.4f}".format(labels[classIDs[i]], confidences[i])
+    #         cv.putText(img, text, (x, y-5), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)  # 字体风格、大小、颜色、粗细
+    return out_boxes, out_confidences, out_classIDs, out_centers
+
+
+def find_button(image, template_list):
+    for template in template_list:
+        result = get_coor(image, template, 0.8)
+        if result is not None:
+            (left_top, right_bottom) = result
+            return left_top, right_bottom
+        
+
+def process_one_battle_frame(image):
+    out_boxes, out_confidences, out_classIDs, out_centers = predict(net, image, yolo_labels)
+    if len(out_centers) > 0:
+        result = find_button(image, template_catch_list)
+        if result is not None:
+            left_top, right_bottom = result
+            center_x = int((left_top[0] + right_bottom[0])/2)
+            center_y = int((left_top[1] + right_bottom[1])/2)
+            move_close_to(center_x, center_y)
+            move_and_click_in_game(center_x, center_y)
+            move_close_to(out_centers[0][0], out_centers[0][1])
+            move_and_click_in_game(out_centers[0][0], out_centers[0][1])
+
+    else:
+        result = find_button(image, template_run_list)
+        if result is not None:
+            left_top, right_bottom = result
+            center_x = int((left_top[0] + right_bottom[0])/2)
+            center_y = int((left_top[1] + right_bottom[1])/2)
+            move_close_to(center_x, center_y)
+            move_and_click_in_game(center_x, center_y)
+
+
+if __name__ == "__main__":
+    video_path = './video/test6.mp4'
+    cap = cv.VideoCapture(video_path)
+    ret, frame = cap.read()
+    while ret:
+        process_one_battle_frame(frame)
+
